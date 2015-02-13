bec621f2b4cfc44ac535e8b08004e62dd75acd22
shoragon <shoragon@gmail.com>
2015-02-13 16:49:17 +0800

    with errors not yet resolved
    SpreadsheetFeed feed = googleService.getFeed(feedUrl,
    SpreadsheetFeed.class);
    List<SpreadsheetEntry> spreadsheets = feed.getEntries();
    
    System.out.println("Response Data:");
    
    System.out.println("=====================================================");
    if(spreadsheets != null) {
    
    // Iterate through all of the spreadsheets returned
    for (SpreadsheetEntry spreadsheet : spreadsheets) {
    //Print the name of each spreadshet
    System.out.println(spreadsheet.getTitle().getPlainText());

--- importdata.java
+++ importdata.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.*;
 
 //import org.apache.commons.logging.Log;
 //import org.apache.commons.logging.LogFactory;
@@ -29,36 +28,48 @@ public class importdata{
 	    * @throws AuthenticationException if the service is unable to validate the
 	    *         oauth2 parameters.
 	    */
-	public void loginOAuth2(String clientID, String clientSecret)
-		       throws AuthenticationException {
-		 String SCOPES = "https://docs.google.com/feeds https://spreadsheets.google.com/feeds";
-		// STEP 1: Set up the OAuth objects
+	   public void loginOAuth2(String clientID, String clientSecret)
+	       throws AuthenticationException {
 
-	    // You first need to initialize a few OAuth-related objects.
-	    // GoogleOAuthParameters holds all the parameters related to OAuth.
-	    // OAuthSigner is responsible for signing the OAuth base string.
-	    GoogleOAuthParameters oauthParameters = new GoogleOAuthParameters();
-	    // Set your OAuth Consumer Key (which you can register at
-	    // https://www.google.com/accounts/ManageDomains).
-	    oauthParameters.setOAuthConsumerKey(clientID);
-	    // Initialize the OAuth Signer.  
-	    OAuthSigner signer = null;
-	    oauthParameters.setOAuthConsumerSecret(clientSecret);
-	    signer = new OAuthHmacSha1Signer();
-	    // Finally create a new GoogleOAuthHelperObject.  This is the object you
-	     // will use for all OAuth-related interaction.
-	     GoogleOAuthHelper oauthHelper = new GoogleOAuthHelper(signer);
-	     
-	     // This method also makes a request to get the unauthorized request token,
+	      String SCOPES = "https://docs.google.com/feeds https://spreadsheets.google.com/feeds";
+	      
+	      // STEP 1: Set up the OAuth objects
+
+	      // You first need to initialize a few OAuth-related objects.
+	      // GoogleOAuthParameters holds all the parameters related to OAuth.
+	      // OAuthSigner is responsible for signing the OAuth base string.
+	      GoogleOAuthParameters oauthParameters = new GoogleOAuthParameters();
+
+	      // Set your OAuth Consumer Key (which you can register at
+	      // https://www.google.com/accounts/ManageDomains).
+	      oauthParameters.setOAuthConsumerKey(clientID);
+
+	      // Initialize the OAuth Signer.  
+	      OAuthSigner signer = null;
+	      oauthParameters.setOAuthConsumerSecret(clientSecret);
+	      signer = new OAuthHmacSha1Signer();
+
+	      // Finally create a new GoogleOAuthHelperObject.  This is the object you
+	      // will use for all OAuth-related interaction.
+	      GoogleOAuthHelper oauthHelper = new GoogleOAuthHelper(signer);
+
+
+	      // STEP 2: Get the Authorization URL
+
+	      // Set the scope for this particular service.
+	      oauthParameters.setScope(SCOPES);
+
+	      // This method also makes a request to get the unauthorized request token,
 	      // and adds it to the oauthParameters object, along with the token secret
 	      // (if it is present).
 	      try {
-	    	  	oauthHelper.getUnauthorizedRequestToken(oauthParameters);
-	      		} catch (OAuthException e) {
+	    oauthHelper.getUnauthorizedRequestToken(oauthParameters);
+	   } catch (OAuthException e) {
 	    // TODO Auto-generated catch block
 	    e.printStackTrace();
-	      		}
-	    // Get the authorization url.  The user of your application must visit
+	   }
+
+	      // Get the authorization url.  The user of your application must visit
 	      // this url in order to authorize with Google.  If you are building a
 	      // browser-based application, you can redirect the user to the authorization
 	      // url.
@@ -68,12 +79,91 @@ public class importdata{
 	          + "request token.  Once that is complete, press any key to "
 	          + "continue...");
 	      try {
-	    	    System.in.read();
-	    	   } catch (IOException e1) {
-	    	    // TODO Auto-generated catch block
-	    	    e1.printStackTrace();
-	    	   }
+	    System.in.read();
+	   } catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	   }
+
+
+	      // STEP 3: Get the Access Token
+
+	      // Once the user authorizes with Google, the request token can be exchanged
+	      // for a long-lived access token.  If you are building a browser-based
+	      // application, you should parse the incoming request token from the url and
+	      // set it in GoogleOAuthParameters before calling getAccessToken().
+	      String token = null;
+	   try {
+	    token = oauthHelper.getAccessToken(oauthParameters);
+	   } catch (OAuthException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	   }
+	      System.out.println("OAuth Access Token: " + token);
+	      System.out.println();
+
+
+	      // STEP 4: Make an OAuth authorized request to Google
+
+	      // Initialize the variables needed to make the request
+	      URL feedUrl = null;
+	   try {
+	    feedUrl = new URL("https://spreadsheets.google.com/feeds/spreadsheets/private/full");
+
+	   } catch (MalformedURLException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	   }
+	      System.out.println("Sending request to " + feedUrl.toString());
+	      System.out.println();
+	      SpreadsheetService googleService =
+	          new SpreadsheetService("oauth-sample-app");
+
+	      // Set the OAuth credentials which were obtained from the step above.
+	      try {
+	    googleService.setOAuthCredentials(oauthParameters, signer);
+	   } catch (OAuthException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	   }
+
+	      // Make the request to Google
+
 	      
-	}
+	      // Make a request to the API and get all spreadsheets.
+	      SpreadsheetFeed feed = googleService.getFeed(feedUrl, SpreadsheetFeed.class);
+	      List<SpreadsheetEntry> spreadsheets = feed.getEntries();
+	      
+	      System.out.println("Response Data:");
+	      System.out.println("=====================================================");
+	      if(spreadsheets != null) {
+	       
+	       // Iterate through all of the spreadsheets returned
+	       for (SpreadsheetEntry spreadsheet : spreadsheets) {
+	   //Print the name of each spreadshet
+	        System.out.println(spreadsheet.getTitle().getPlainText());
+	       }
+	      }
+
+	       System.out.println("=====================================================");
+	      System.out.println();
+
+	     
+
+
+	      ////////////////////////////////////////////////////////////////////////////
+	      // STEP 5: Revoke the OAuth token
+	      ////////////////////////////////////////////////////////////////////////////
+
+	      System.out.println("Revoking OAuth Token...");
+	      try {
+	    oauthHelper.revokeToken(oauthParameters);
+	   } catch (OAuthException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	   }
+	      System.out.println("OAuth Token revoked...");
+	   }
+
 
 }
